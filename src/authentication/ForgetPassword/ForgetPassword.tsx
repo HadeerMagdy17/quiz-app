eafaadba8dc10d472fc7b7c9cc431d774efe4ec4
rahma-nagy <rahmanagy11sep@gmail.com>
2024-02-02 09:12:35 -0800

    forget-pass ui

--- src/authentication/ForgetPassword/ForgetPassword.tsx
+++ src/authentication/ForgetPassword/ForgetPassword.tsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import bgAuth from "../../assets/image.png";
+import Logo from "../../assets/Logo.png";
 
 const ForgetPassword = () => {
   return (
-    <div>ForgetPassword</div>
+    <>
+     <div className="container mx-auto " style={{overflow:'hidden'}}>
+        <form className="flex items-center justify-center min-h-screen bg-slate-950 p-12">
+          <div className="flex-1 ml-auto">
+          <img src={Logo} alt="Quizwiz" className='pt-5'/>
+            <p className="text-lime-300 p-5">Forgot password</p>
+            <div className="w-2/3 md:w-1/2 mt-5">
+              <label htmlFor="email" className="text-white mt-5 ">Registered email address</label>
+              
+              <input
+                id="email"
+                type="email"
+                className="w-80 bg-slate-950 text-white p-2 mb-2 border border-white rounded-md"
+                placeholder="Type your email"
+              />
+
+            </div>
+
+            <button
+              type="submit"
+              className="w-40 md:w-40 bg-white text-slate-950 hover:bg-white p-2 font-semibold mt-5 rounded-md"
+            >
+              Sign In
+            </button>
+          </div>
+
+          <div className="flex-1 hidden md:flex items-center">
+            <img src={bgAuth} alt="bg" width="70%" />
+          </div>
+        </form>
+      </div> 
+    </>
   )
 }
 
--- src/authentication/Login/Login.tsx
+++ src/authentication/Login/Login.tsx
@@ -6,21 +6,21 @@ import Options from '../../assets/Options.png'
 const Login = () => {
   return (
     <>
-    <div className="container mx-auto">
+    <div className="container mx-auto " style={{overflow:'hidden'}}>
         <form className="flex items-center justify-center min-h-screen bg-slate-950 p-12">
           <div className="flex-1 ml-auto">
             <img src={Logo} alt="Quizwiz" />
-            <p className="text-lime-300">Continue your learning journey with QuizWiz!</p>
+            <p className="text-lime-300 pt-5">Continue your learning journey with QuizWiz!</p>
 
-            <img src={Options} alt="#"/>
+            <img src={Options} alt="#" className="pt-5"/>
 
             <div className="w-2/3 md:w-1/2">
-              <label htmlFor="email" className="text-white">Registered email address</label>
-
+              <label htmlFor="email" className="text-white ">Registered email address</label>
+              
               <input
                 id="email"
                 type="email"
-                className="w-full bg-slate-950 text-white p-2 mb-2 border border-white rounded-md"
+                className="w-80 bg-slate-950 text-white p-2 mb-2 border border-white rounded-md"
                 placeholder="Type your email"
               />
 
@@ -28,20 +28,20 @@ const Login = () => {
               <input
                 id="password"
                 type="password"
-                className="w-full bg-slate-950 text-white p-2 mb-2 border border-white rounded-md"
+                className="w-80 bg-slate-950 text-white p-2 mb-2 border border-white rounded-md"
                 placeholder="Type your password"
               />
             </div>
 
             <button
               type="submit"
-              className="w-40 bg-white text-slate-950 hover:bg-white p-2 mt-6 font-semibold rounded-md"
+              className="w-40 md:w-40 bg-white text-slate-950 hover:bg-white p-2 font-semibold rounded-md"
             >
               Sign In
             </button>
           </div>
 
-          <div className="flex-1 flex items-center">
+          <div className="flex-1 hidden md:flex items-center">
             <img src={bgAuth} alt="bg" width="70%" />
           </div>
         </form>
