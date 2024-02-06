1f014ffaa29c8ca1d09ad75463534f353196d945
rahma-nagy <rahmanagy11sep@gmail.com>
2024-02-05 21:21:00 -0800

    edit btn

--- src/authentication/Register/Register.tsx
+++ src/authentication/Register/Register.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import bgAuth from "../../assets/image.png";
 import Logo from "../../assets/Logo.png";
-import OptionRegister from "../../assets/OptionRegister.png";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const {
@@ -18,7 +18,51 @@ const Register = () => {
           Continue your learning journey with QuizWiz!
         </p>
 
-        <img src={OptionRegister} alt="#" className="pb-2" />
+        <div className="flex mt-3">
+          <Link to="/" className="signin w-1/2">
+            <div className="content flex flex-col items-center text-8xl py-3 bg-stone-700 me-3 rounded-lg text-center border-4 ">
+              <i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                  />
+                </svg>
+              </i>
+              <p className="text-base mt-2">Sign in</p>
+            </div>
+          </Link>
+          <div className="signup w-1/2">
+            <div className="content flex flex-col items-center text-8xl py-3 bg-stone-700 me-3 rounded-lg text-center border-4  border-lime-300">
+              <i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+                  />
+                </svg>
+              </i>
+              <p className="text-base mt-2">Sign Up</p>
+            </div>
+          </div>
+          <div className="signup w-1/2"></div>
+        </div>
 
         <form onSubmit={handleSubmit()} className="w-full relative">
           <div className="flex space-x-4">
