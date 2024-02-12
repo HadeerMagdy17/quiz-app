3438eb0a9763831170603225d46adca785237d20
Hadeer Magdy <dody.magdy711@gmail.com>
2024-02-12 16:44:27 +0200

    add toast to delete student

--- src/Redux/Features/Instructor/Students/DeleteStudentsSlice.tsx
+++ src/Redux/Features/Instructor/Students/DeleteStudentsSlice.tsx
@@ -1,6 +1,7 @@
 import { deleteStudentUrl, requestHeaders } from "../../../../Services/api";
 import axios from "axios";
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 export const deleteStudent = createAsyncThunk(
   "delete/deleteStudent",
@@ -8,14 +9,25 @@ export const deleteStudent = createAsyncThunk(
     const { rejectWithValue } = thunkAPI;
     try {
         const response = await axios.delete(`${deleteStudentUrl}/${id}`,{
-            headers: requestHeaders,
-            
+            headers: requestHeaders,       
     
         }
+        
       );
+      toast.success(response.data.message, {
+        autoClose: 2000,
+        theme: "colored",
+    });
       return response;
     } catch (error) {
-
+      toast.error(
+        
+        error.response?.data?.message || "An error occurred during login.",
+        {
+            autoClose: 2000,
+            theme: "colored",
+        }
+    );
       return rejectWithValue(error?.message);
     }
   }
