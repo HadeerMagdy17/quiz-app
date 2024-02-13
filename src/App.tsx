177cb8b278da9700961384ea32a34f6f5a4782da
Hadeer Magdy <dody.magdy711@gmail.com>
2024-02-13 15:19:05 +0200

    add learner quizess,add user role in side bar and handel instructor and students view

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import './App.css'
-import MasterLayout from './Shared/MasterLayout/MasterLayout';
-import { Notfound } from './Shared/Notfound/Notfound';
-import ForgetPassword from './authentication/ForgetPassword/ForgetPassword';
-import Login from './authentication/Login/Login';
-import Register from './authentication/Register/Register';
-import { AuthLayout } from './Shared/AuthLayout/AuthLayout';
-import { ToastContainer } from 'react-toastify';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import MasterLayout from "./Shared/MasterLayout/MasterLayout";
+import { Notfound } from "./Shared/Notfound/Notfound";
+import ForgetPassword from "./authentication/ForgetPassword/ForgetPassword";
+import Login from "./authentication/Login/Login";
+import Register from "./authentication/Register/Register";
+import { AuthLayout } from "./Shared/AuthLayout/AuthLayout";
+import { ToastContainer } from "react-toastify";
 
-import Quizzes from './Features/Instructor/Quizzes/Quizzes';
-import {QuizzesDetails}  from './Features/Instructor/Quizzes/QuizzesDetails/QuizzesDetails.tsx';
+import Quizzes from "./Features/Instructor/Quizzes/Quizzes";
+import { QuizzesDetails } from "./Features/Instructor/Quizzes/QuizzesDetails/QuizzesDetails.tsx";
 // import Questions from './Features/Instructor/Quizzes/Questions/Questions';
 
-import ResetPassword from './authentication/ResetPassword/ResetPassword';
+import ResetPassword from "./authentication/ResetPassword/ResetPassword";
 
-import ChangePassword from './authentication/ChangePassword/ChangePassword';
+import ChangePassword from "./authentication/ChangePassword/ChangePassword";
 
+import Groups from "./Features/Instructor/Groups/Groups";
 
+import Dashboard from "./Features/Instructor/Dashboard/Dashboard";
+import Students from "./Features/Instructor/Students/Students";
 
-import Groups from './Features/Instructor/Groups/Groups';
+import { Provider } from "react-redux";
+import store from "./Redux/Store.tsx";
 
-import Dashboard from './Features/Instructor/Dashboard/Dashboard';
-import Students from './Features/Instructor/Students/Students';
-
-import { Provider } from 'react-redux';
-import store from './Redux/Store.tsx'
-
-import Results from './Features/Instructor/Results/Results';
-import ViewResults from './Features/Instructor/ViewResults/ViewResults';
-
-
+import Results from "./Features/Instructor/Results/Results";
+import ViewResults from "./Features/Instructor/ViewResults/ViewResults";
+import LearnerQuizzes from "./Features/Learner/LearnerQuizzes/LearnerQuizzes";
 
 function App() {
   const routes = createBrowserRouter([
@@ -45,48 +42,36 @@ function App() {
         { path: "notfound", element: <Notfound /> },
         { path: "change-password", element: <ChangePassword /> },
         { path: "reset-password", element: <ResetPassword /> },
-
       ],
     },
     {
       path: "dashboard",
 
-      element: (
-
-        <MasterLayout />
-
-      ),
+      element: <MasterLayout />,
       errorElement: <Notfound />,
       children: [
-
         { index: true, element: <Dashboard /> },
         { path: "students", element: <Students /> },
         { path: "groups", element: <Groups /> },
         { path: "results", element: <Results /> },
         { path: "results/viewresults", element: <ViewResults /> },
-
-
-        // { path: "projects/add-project", element: <AddProject /> },
         { path: "quizzes", element: <Quizzes /> },
-        { path: "quizzes/quiz-details", element: <QuizzesDetails/> },
-        // { path: "quizzes/questions", element: <Questions/> },
-        // { path: "tasks", element: <Tasks /> },
-        // { path: "tasks/add-task", element: <AddTask /> },
+        { path: "quizzes/quiz-details", element: <QuizzesDetails /> },
+        { path: "learnerquiz", element: <LearnerQuizzes /> },
       ],
     },
   ]);
-
 
   return (
     <>
       <div>
         <Provider store={store}>
           <RouterProvider router={routes} />
-          {/* <ToastContainer /> */}
+         
         </Provider>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
