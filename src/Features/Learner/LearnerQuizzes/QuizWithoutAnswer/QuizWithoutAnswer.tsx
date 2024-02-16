4293126d3c1aaa246b14cc210d3e45aea5f06c7d
Hadeer Magdy <dody.magdy711@gmail.com>
2024-02-16 17:45:03 +0200

    add quiz question preview

--- src/Features/Learner/LearnerQuizzes/QuizWithoutAnswer/QuizWithoutAnswer.tsx
+++ src/Features/Learner/LearnerQuizzes/QuizWithoutAnswer/QuizWithoutAnswer.tsx
@@ -1,57 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchQuestionsWithoutAnswers } from '../../../../Redux/Features/Learner/QuestionsWithoutAnswerSlice';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchQuestionsWithoutAnswers } from "../../../../Redux/Features/Learner/QuestionsWithoutAnswerSlice";
 
 const QuizWithoutAnswer = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answers, setAnswers] = useState({});
 
-
   const dispatch = useDispatch();
 
-  const { questions, loading, error } = useSelector(state => state.questionWithoutAnswers);
-  console.log(questions);
+  const {
+    questions: quiz,
+    loading,
+    error,
+  } = useSelector((state) => state.questionWithoutAnswers);
+  console.log(quiz);
+  console.log(quiz?.questions);
+  console.log(quiz?.title);
 
   useEffect(() => {
     dispatch(fetchQuestionsWithoutAnswers("65cf3f748f25f1b30030c90b"));
   }, [dispatch]);
 
+  //   const handleAnswerSelect = (questionId, selectedAnswer) => {
+  //     setAnswers(prevState => ({
+  //       ...prevState,
+  //       [questionId]: selectedAnswer
+  //     }));
+  //   };
 
-  
+  //   const checkAnswer = (questionId) => {
+  //     return answers[questionId] === questions.find(question => question.id === questionId).correctAnswer;
+  //   };
 
-//   const handleAnswerSelect = (questionId, selectedAnswer) => {
-//     setAnswers(prevState => ({
-//       ...prevState,
-//       [questionId]: selectedAnswer
-//     }));
-//   };
+  //   const nextQuestion = () => {
+  //     setCurrentQuestionIndex(prevIndex => prevIndex + 1);
+  //   };
 
-//   const checkAnswer = (questionId) => {
-//     return answers[questionId] === questions.find(question => question.id === questionId).correctAnswer;
-//   };
-
-//   const nextQuestion = () => {
-//     setCurrentQuestionIndex(prevIndex => prevIndex + 1);
-//   };
-
-//   const currentQuestion = questions[currentQuestionIndex];
+  //   const currentQuestion = questions[currentQuestionIndex];
 
   return (
-    <div style={{backgroundColor:"red"}}>
-      {Array.isArray(questions) && questions.map((question, index) => (
+    <div className="pl-5" style={{ backgroundColor: "#fbfbfb", justifyItems: "center" }}>
+      {quiz?.questions.map((question, index) => (
         <div key={question._id}>
-          <p>{index + 1}. {question?.questions}</p>
-          <p>rawan</p>
+          <p>
+            {index + 1}.{/* {question?.questions} */}
+          </p>
+
           <form>
-            {Object.entries(question?.options).map(([optionKey, optionValue]) => (
-              <div key={optionKey}>
-                <input type="radio" id={optionKey} name={`question_${index}`} value={optionKey} />
-                <label htmlFor={optionKey}>{optionValue}</label>
-              </div>
-            ))}
+            {Object.entries(question?.options).map(
+              ([optionKey, optionValue]) => (
+                <div key={optionKey}>
+                  <input
+                    type="radio"
+                    id={optionKey}
+                    name={`question_${index}`}
+                    value={optionKey}
+                  />
+                  <label htmlFor={optionKey}>{optionValue}</label>
+                </div>
+              )
+            )}
           </form>
         </div>
       ))}
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 rounded">
+        Next
+      </button>
     </div>
   );
 };
