76b833d5e95283d65d40a06bb859ec6a041ae65e
rawan.mohamed7129@gmail.com <rawan.mohamed7129@gmail.com>
2024-02-18 02:51:55 +0200

    Display Data

--- src/Redux/Features/Instructor/Quizes/QuizesDetaiksSlice.tsx
+++ src/Redux/Features/Instructor/Quizes/QuizesDetaiksSlice.tsx
@@ -1,58 +1,120 @@
-import { quizDetailsUrl, requestHeaders } from "../../../../Services/api";
+// import { quizDetailsUrl, requestHeaders } from "../../../../Services/api";
+// import axios from "axios";
+// import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+
+// interface DetailsProps {
+//   details: {},  // Make sure details is initialized to an object
+//   loading: boolean;
+//   error: null | string;
+// }
+
+// const initialState: DetailsProps = {
+//   details: {},  // Make sure details is initialized to an object
+//   loading: false,
+//   error: null,
+// };
+
+
+
+// export const quizDetails = createAsyncThunk(
+//   "details/quizDetails",
+//   async ( id ) => {
+
+//     // eslint-disable-next-line no-useless-catch
+//     try {
+//       const response = await axios.get(`${quizDetailsUrl}/${id}`, {
+//         headers: requestHeaders,
+//       }
+
+//       );
+
+//       return response?.data;
+//     } catch (error) {
+//       throw error
+//       //   return rejectWithValue(error?.message);
+//     }
+//   }
+// );
+// const detailsItemSlice = createSlice({
+//   name: "details",
+//   initialState,
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(quizDetails.pending, (state) => {
+//         state.loading = true;
+//       })
+//       .addCase(quizDetails.fulfilled, (state, action: PayloadAction<any>) => {
+//         state.loading = false;
+//         state.details = action.payload.data;
+//       })
+//       .addCase(quizDetails.rejected, (state, action: PayloadAction<any>) => {
+//         state.loading = false;
+//         state.error = action.payload.message;
+//       });
+//   },
+// });
+
+// export default detailsItemSlice.reducer;
+
+
+
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
-import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-
-
+import { quizDetailsUrl, requestHeaders } from "../../../../Services/api";
 
 interface DetailsProps {
-    details: {},  // Make sure details is initialized to an object
-    loading: boolean;
-    error: null | string;
-  }
+  details: {},  // Make sure details is initialized to an object
+  loading: boolean;
+  error: null | string;
+}
 
-  const initialState: DetailsProps = {
-    details: {},  // Make sure details is initialized to an object
-    loading: false,
-    error: null,
-  };
 
-  export const quizDetails = createAsyncThunk(
-    "details/quizDetails",
-    async ({ id }, thunkAPI) => {
-    const { rejectWithValue } = thunkAPI;
-    try {
-        const response = await axios.get(`${quizDetailsUrl}/${id}`,{
-            headers: requestHeaders,       
-        }
-        
-      );
-     
-      return response?.data;
-    } catch (error) {       
-      throw error
-    //   return rejectWithValue(error?.message);
-    }
-  }
-);
 
-const detailsItemSlice = createSlice({
-  name: "details",
+const initialState: DetailsProps = {
+  details: {},  // Make sure details is initialized to an object
+  loading: false,
+  error: null,
+};
+
+
+export const QuizesDetaiksSlice = createSlice({
+  name: "DetailsQuizSlice",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder
-      .addCase(quizDetails.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(quizDetails.fulfilled, (state, action: PayloadAction<any>) => {
+    builder.addCase(quizDetails.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(
+      quizDetails.fulfilled,
+      (state, action: PayloadAction<any>) => {
         state.loading = false;
         state.details = action.payload;
-      })
-      .addCase(quizDetails.rejected, (state, action: PayloadAction<any>) => {
-        state.loading = false;
-        state.error = action.payload.message;
-      });
+        // state.details.data = action.payload
+        // state.details.data = action.payload;
+      }
+    );
+    builder.addCase(quizDetails.rejected, (state, action: PayloadAction<any>) => {
+      state.loading = false;
+      state.error = action.payload.message;
+    });
   },
 });
 
-export default detailsItemSlice.reducer;
+export const quizDetails = createAsyncThunk<any, string>(
+  "DetailsQuizSlice/quizDetails",
+  async (quizId: string) => {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      const response = await axios.get(`${quizDetailsUrl}/${quizId}`, {
+        headers: requestHeaders,
+      });
+      return response?.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+);
+export default QuizesDetaiksSlice.reducer;
